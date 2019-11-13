558db3cfeeeb0bed6681102b081405fef514bda2
patrick hoehn <patrh_99@hotmail.com>
2019-11-13 18:55:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,7 +4,19 @@ def map(array)
   i = 0 
   while i < array.length do
     new_array.push(yield(array[i]))
-    i +=1
+    i += 1
   end
   new_array
+end
+
+def reduce(array, starting_point = 0)
+  new_array = []
+  total = 0
+  total += starting_point
+  
+  i = 0
+  if i < array.length do
+    
+  end
+  
 end
